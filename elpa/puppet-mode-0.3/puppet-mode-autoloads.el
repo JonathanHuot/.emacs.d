35349f9fc60988a7a1e01446b9cf48971ee974e3
Jonathan Huot <jonathan.huot@gmail.com>
2016-03-09 20:56:12 +0100

    Added puppet-mode and magit

--- elpa/puppet-mode-0.3/puppet-mode-autoloads.el
+++ elpa/puppet-mode-0.3/puppet-mode-autoloads.el
@@ -3,8 +3,13 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
+<<<<<<< 4d5035a3e8d40ee8bb0d88ae34cc57237862ab0e
 ;;;### (autoloads nil "puppet-mode" "puppet-mode.el" (22298 16586
 ;;;;;;  555694 140000))
+=======
+;;;### (autoloads nil "puppet-mode" "puppet-mode.el" (22195 50499
+;;;;;;  862745 916000))
+>>>>>>> Added puppet-mode and magit
 ;;; Generated autoloads from puppet-mode.el
 
 (autoload 'puppet-mode "puppet-mode" "\
--- init.el
+++ init.el
@@ -31,8 +31,8 @@
   (package-initialize)
   (expand-file-name "~/.emacs.d/elpa/package.el")
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
-  (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
-  (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;;  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+  (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
   )
 
 ;; 
