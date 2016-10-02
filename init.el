6c8e79bc7ec076efd3e6cc7e09e42ff66e16eeff
Jonathan Huot <jonathan.huot@gmail.com>
2016-10-02 15:37:27 +0200

    Added md, js2, dockerfile modes

--- init.el
+++ init.el
@@ -181,40 +181,32 @@
 
 ;:background gray2
 (custom-set-faces
- '(default ((t (:overline nil :inherit nil :stipple nil :background "#242424"
-                :foreground "#FFF991" :inverse-video nil :box nil
-                :strike-through nil :underline nil
-                :foundry "unknown" :family "DejaVu Sans Mono"))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:overline nil :inherit nil :stipple nil :background "#242424" :foreground "#FFF991" :inverse-video nil :box nil :strike-through nil :underline nil :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(border ((t nil)))
  '(cursor ((t (:background "firebrick1" :foreground "black"))))
- '(font-lock-comment-delimiter-face
-    ((default (:inherit font-lock-comment-face :weight ultra-bold))
-    (((class color) (min-colors 16)) nil)))
+ '(font-lock-comment-delimiter-face ((default (:inherit font-lock-comment-face :weight ultra-bold)) (((class color) (min-colors 16)) nil)))
  '(font-lock-comment-face ((t (:foreground "lime green"))))
  '(font-lock-doc-face ((t (:foreground "tomato" :slant italic))))
- '(font-lock-function-name-face
-    ((t (:foreground "deep sky blue" :underline t :weight bold))))
+ '(font-lock-function-name-face ((t (:foreground "deep sky blue" :underline t :weight bold))))
  '(font-lock-keyword-face ((t (:foreground "gold" :weight bold))))
  '(font-lock-string-face ((t (:foreground "tomato" :slant italic))))
  '(fringe ((nil (:background "black"))))
- '(highlight ((t (:background "khaki1" :foreground "black"
-                  :box (:line-width -1 :color "firebrick1")))))
+ '(highlight ((t (:background "khaki1" :foreground "black" :box (:line-width -1 :color "firebrick1")))))
  '(highlight-current-line-face ((t (:inherit highlight))))
  '(lazy-highlight ((t (:background "paleturquoise" :foreground "black"))))
  '(link ((t (:foreground "DodgerBlue3" :underline t))))
  '(menu ((t (:background "gray2" :foreground "#FFF991"))))
  '(minibuffer-prompt ((t (:foreground "royal blue"))))
- '(mode-line ((t (:background "dark olive green"
-                  :foreground "dark blue"
-                  :box (:line-width -1 :color "gray75")
-                  :weight bold))))
+ '(mode-line ((t (:background "dark olive green" :foreground "dark blue" :box (:line-width -1 :color "gray75") :weight bold))))
  '(mode-line-buffer-id ((t (:background "dark olive green" :foreground "beige"))))
  '(mode-line-highlight ((((class color) (min-colors 88)) nil)))
- '(mode-line-inactive ((t (:background "dark olive green"
-                           :foreground "dark khaki" :weight light))))
+ '(mode-line-inactive ((t (:background "dark olive green" :foreground "dark khaki" :weight light))))
  '(mouse ((t (:background "Grey" :foreground "black"))))
- '(trailing-whitespace ((((class color) (background dark))
-                          (:background "firebrick1")))))
+ '(trailing-whitespace ((((class color) (background dark)) (:background "firebrick1")))))
 
 ; make sure the frames have the dark background mode by default
 (setq default-frame-alist (quote (
@@ -258,3 +250,11 @@
                    name (file-name-nondirectory new-name)))))))
 
 (global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (dockerfile-mode markdown-mode org js2-mode yaml-mode vcl-mode puppet-mode php-mode magit haml-mode groovy-mode flymake-php color-theme cmake-mode auto-complete-c-headers))))
